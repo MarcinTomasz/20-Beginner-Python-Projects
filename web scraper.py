ed61b0c1772219c8da57dfc9f7f8c42880ea13c4
MTM <mtmalek@protonmail.com>
2022-09-19 18:42:25 +0200

    Web scraper

--- web scraper.py
+++ web scraper.py
@@ -3,8 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.immowelt.at/'
+url = 'https://www.codewithtomi.com/'
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'lxml')
 title = soup.find_all('h2', {'class': 'post-title'})
+title
